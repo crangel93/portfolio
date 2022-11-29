6af7d4874096864e4463fcae1fe2963ebc6d7227
Christian Rangel <ChristianRangel@Christians-MacBook-Air.local>
2022-11-29 15:00:14 -0800

    created componenets

--- src/components/About.js
+++ src/components/About.js
@@ -31,9 +31,9 @@ function About(){
         <div className="contact" id="contact" style={{style2}}>
           <h3>Contact Me</h3>
           <p>Phone: (209) 678-2653</p>
-          <p>Email:<a href="mailto:christian.j.rangel93@gmail.com" style={{style3}}>christian.j.rangel93@gmail.com</a></p>
+          <p>Email:<a href="mailto:christian.j.rangel93@gmail.com" style={{style3}}>christian.j.rangel93@gmail.com</a> LinkedIn:<a href="www.linkedin.com/in/christianjrangel1993" style={{style4}}>LinkedIn</a></p>
           <p>
-            <a href="www.linkedin.com/in/christianjrangel1993" style={{style4}}>LinkedIn</a>
+            
           </p>
         </div>
          <img src={modesto} alt="" style={{width:"100%" ,display:"inline"}}/>
