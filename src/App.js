0cc1b04d99934f37e2b9c6488d2bcb351bdab6f4
Christian Rangel <ChristianRangel@Christians-MacBook-Air.local>
2022-11-29 15:18:47 -0800

    finishing changes

--- src/App.js
+++ src/App.js
@@ -1,24 +1,34 @@
-import logo from './logo.svg';
 import './App.css';
 
+import cbc from "./images/covidByCountry.png"
+
+import horsien from "./images/horiseon.png"
+
+
+
+import modesto from "./images/modesto.jpg"
+import profile from "./images/profile.png"
+import runBuddy from "./images/runbuddy.png"
+import Untitled from "./images/Untitled.png"
+import Header from './components/Header'
+import Projects from './components/Projects'
+import About from './components/About'
+import Footer from './components/Footer'
+
 function App() {
+  let style1 = 'display:inline; float:left; border:1px solid black; border-radius:4%; text-align:center; width:250px;'
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+
+
+      <Header />
+      <About />
+      <Projects />
+      <Footer />
+
     </div>
+
   );
 }
 
